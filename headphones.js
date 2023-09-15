a0fe4cd19a89c023cf8a8b1b864ccc267bd8a18e
ArtLegends <norik.artemka@gmail.com>
2023-09-15 22:57:27 +0300

    .

--- headphones.js
+++ headphones.js
@@ -1,3 +1,3 @@
 function redirectToPreorder() {
-    window.location.href = "https://th-store.org/products/logitech-g733-lightspeed-wireless-rgb-gaming-headset";
+    window.location.href = "https://tm-market.com/en/products/logitech-g733-lightspeed-wireless-rgb-gaming-headset";
 }
